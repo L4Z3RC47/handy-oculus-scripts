c8de9893ca6a51765a9fb86e74d40e1dfcc33862
William Robertson <wrobertson@c-k.com>
2019-01-09 13:58:19 -0600

    adding audio spectrum script to scripts

---
+++ Handy_Scripts/Audio/AudioSourceGetSpectrumDataExample.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSourceGetSpectrumDataExample : MonoBehaviour
+{
+    public float gain;
+    void Update()
+    {
+        float[] spectrum = new float[128];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            //Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+            //Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1)*2, spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i)*2, spectrum[i] - 10, 1), Color.green);
+            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+            //Debug.DrawLine(new Vector3((i), 0, 0), new Vector3((i), spectrum[i], 0), Color.blue);
+            DrawLine(new Vector3(((i/2) - (spectrum.Length/4)), 0, 0), new Vector3(((i/2) - (spectrum.Length / 4)), spectrum[i]*gain, 0), Color.red);
+        }
+    }
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.05f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.SetColors(color, color);
+        lr.SetWidth(0.5f, 0.5f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+}
